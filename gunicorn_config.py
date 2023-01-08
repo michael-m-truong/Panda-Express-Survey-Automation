57c1864c2f14ee2984c5cb77005c92027f874ad4
Michael Truong <truong.minh.michael@gmail.com>
2023-01-07 19:03:19 -0800

    lower worker count

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,7 @@
-import multiprocessing
-
 bind = "0.0.0.0:5000"
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = workers*2
+#workers = multiprocessing.cpu_count() * 2 + 1
+workers = 4
+threads = workers
 timeout = 120
 worker_class = "gevent"
 preload_app = True
