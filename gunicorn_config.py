1a847735e458d5079db65af4af10924846e216fa
Michael Truong <truong.minh.michael@gmail.com>
2023-01-07 16:17:37 -0800

    testing config settings

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,8 @@
+import multiprocessing
+
 bind = "0.0.0.0:5000"
-workers = 3
-threads = 3
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = workers*2
 timeout = 120
+worker_class = "gevent"
+preload_app = True
