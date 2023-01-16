e8245ead1f784d375b96ebeee49e98345b282bfd
Michael Truong <truong.minh.michael@gmail.com>
2023-01-16 14:55:59 -0800

    added more workers

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,10 @@
+import multiprocessing
+
 bind = "0.0.0.0:5000"
-#workers = multiprocessing.cpu_count() * 2 + 1
-workers = 4
+workers = multiprocessing.cpu_count() * 2 + 1
+#workers = 4
 threads = workers
 timeout = 120
-worker_class = "sync"
+#worker_class = "sync"
 #graceful_timeout = 0
 preload_app = True
