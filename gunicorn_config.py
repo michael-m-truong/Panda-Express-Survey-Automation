e8d751426bf9a7986d9b9040816e3621c3a2fb81
Michael Truong <truong.minh.michael@gmail.com>
2023-01-17 16:53:48 -0800

    slightly lower worker count to fix crash

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = "0.0.0.0:5000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 2 - 5
 #workers = 4
 threads = workers
 timeout = 120
