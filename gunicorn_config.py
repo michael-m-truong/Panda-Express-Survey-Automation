b6664af04a291f7b710fb359a3a635138ef64860
Michael Truong <truong.minh.michael@gmail.com>
2023-01-19 16:08:48 -0800

    use gevent worker

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,10 @@
-import multiprocessing
+import gevent
 
 bind = "0.0.0.0:5000"
-workers = multiprocessing.cpu_count() * 2 - 5
+#workers = multiprocessing.cpu_count() * 2 - 5
 #workers = 4
-threads = workers
-timeout = 120
-#worker_class = "sync"
+#threads = workers
+timeout = 240 #120
+worker_class = "gevent"
 #graceful_timeout = 0
 preload_app = True
